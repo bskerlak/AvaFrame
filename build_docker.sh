dc23e291b5b3d144b5f27bbf867f07962f8322cc
Bojan Skerlak <bojan@skerlak.ch>
2025-12-28 16:29:44 +0100

    version bump to 0.0.4

--- build_docker.sh
+++ build_docker.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 docker build \
-  --build-arg AVAFRAME_VERSION=0.0.3 \
-  -t avaframe:0.0.3 \
+  --build-arg AVAFRAME_VERSION=0.0.4 \
+  -t avaframe:0.0.4 \
   -t avaframe:latest \
   .
